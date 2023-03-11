7a534f2777fbcb9d07385b52285602a92d517925
MyLanOPS <126709662+MyLanOPS@users.noreply.github.com>
2023-03-11 10:44:50 +0100

    test

--- src/app.py
+++ src/app.py
@@ -8,6 +8,7 @@ def handler(event, context):
     df_san = ah.get_ohlcv_data('SAN')
     print(df_san)
 
+
     # Llamar al algo
     eq_w_1 = EqwAlgo(algo_tag='test_user_1_miax10_algo3', n_days=10)
     eq_w_1.run_day_comp()
